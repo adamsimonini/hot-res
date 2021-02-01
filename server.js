ad2e39bb1505241a9174c3f8d36662b88ac6a87e
Adam Simonini <adamsimonini@users.noreply.github.com>
2021-02-01 14:59:48 -0500

    added a database (an array) and added an endpoint for getting the array of tables

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
 // get the express package, and get path for later
-const express = require('express');
+const express = require("express");
 const path = require("path");
 
 // set app to be equal to the express package
@@ -12,11 +11,37 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// perform an initial get request at the root directory to ensure everything is working
-app.get('/', (req, res) => {
-    res.send('Welcome!')
-})
+// Data
+// ===========================================================
+let tables = [
+    {
+      customerName: "Ahmed",
+      customerEmail: "afhaque89@gmail.com",
+      customerID: "afhaque89",
+      phoneNumber: "979-587-0887",
+    },
+    {
+      customerName: "Adam",
+      customerEmail: "ajsim@gmail.com",
+      customerID: "ajsim99",
+      phoneNumber: "416-499-1165",
+    },
+  ];
+  
 
+// Routes
+// ===========================================================
+// perform an initial get request at the root directory to ensure everything is working
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.get("/api/tables", (req, res) => {
+    res.send(tables);
+});
+
+// Listener
+// ===========================================================
 app.listen(PORT, () => {
-    console.log(`This server is running on port ${PORT}`);
-})
+  console.log(`This server is running on port ${PORT}`);
+});
