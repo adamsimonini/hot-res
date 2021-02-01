3704805639da1ea2d2bda91507581277d9776ff4
Adam Simonini <adamsimonini@users.noreply.github.com>
2021-02-01 16:33:15 -0500

    Endpoint for POSTing table data set. Push incoming table to tables array or waitlist array depending on conditional

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 // assign a port, and be sure to check for a process.env.PORT first (for foreign hosting of this app)
 const PORT = process.env.PORT || 3000;
 
+global.maxTables = 5;
+
 // ensure my upcoming post routes can handle requests using url encoding and json to pass in data to my app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,20 +16,33 @@ app.use(express.json());
 // Data
 // ===========================================================
 let tables = [
-    {
-      customerName: "Ahmed",
-      customerEmail: "afhaque89@gmail.com",
-      customerID: "afhaque89",
-      phoneNumber: "979-587-0887",
-    },
-    {
-      customerName: "Adam",
-      customerEmail: "ajsim@gmail.com",
-      customerID: "ajsim99",
-      phoneNumber: "416-499-1165",
-    },
-  ];
-  
+  {
+    customerName: "Ahmed",
+    customerEmail: "afhaque89@gmail.com",
+    customerID: "afhaque89",
+    phoneNumber: "979-587-0887",
+  },
+  {
+    customerName: "Adam",
+    customerEmail: "ajsim@gmail.com",
+    customerID: "ajsim99",
+    phoneNumber: "416-499-1165",
+  },
+  {
+    customerName: "Ahmed",
+    customerEmail: "afhaque89@gmail.com",
+    customerID: "afhaque89",
+    phoneNumber: "979-587-0887",
+  },
+  {
+    customerName: "Adam",
+    customerEmail: "ajsim@gmail.com",
+    customerID: "ajsim99",
+    phoneNumber: "416-499-1165",
+  },
+];
+
+let waitlist = [];
 
 // Routes
 // ===========================================================
@@ -37,7 +52,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/tables", (req, res) => {
-    res.send(tables);
+  res.send(tables);
+});
+
+app.get("/api/waitlist", (req, res) => {
+  res.send(waitlist);
+});
+
+// Create table - takes in JSON input
+app.post("/api/table", function (req, res) {
+  // This works because of our body parsing middleware
+  var table = req.body;
+  console.log(table);
+  // check if we have enough tables, and if not waitlist the incoming request
+  if (tables.length < global.maxTables) {
+    tables.push(table);
+        // We then display the JSON to the users (res.json(ends the response))
+    res.json(table);
+  } else {
+    console.log("Waitlisting....");
+    console.log(table);
+    waitlist.push(table);
+    // We then display the JSON to the users (res.json(ends the response))
+    res.json(table);
+  }
 });
 
 // Listener
